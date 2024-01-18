32abea47089adc18f5b68c57c0feddcfcdf1ab0a
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-01-19 00:56:23 +1000

    Removed font from Xresources for being tempermental

--- config.def.h
+++ config.def.h
@@ -7,10 +7,8 @@ static int centered  = 0;   /* -c option; centers dmenu on screen */
 static int min_width = 500; /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static       char font[]   = "monospace:size=10";
 static const char *fonts[] = {
-	font,
-	"monospace:size=10"
+	"Iosevka NFM Medium:size=18"
 };
 
 static char *prompt = NULL; /* -p option; prompt to the left of input field */
@@ -46,7 +44,6 @@ ResourcePref resources[] = {
 	{ "fuzzy",        INTEGER, &fuzzy        },
 	{ "centered",     INTEGER, &centered     },
 	{ "min_width",    INTEGER, &min_width    },
-	{ "font",         STRING,  &font         },
 	{ "col_norm_fg",  STRING,  &col_norm_fg  },
 	{ "col_norm_bg",  STRING,  &col_norm_bg  },
 	{ "col_sel_fg",   STRING,  &col_sel_fg   },
