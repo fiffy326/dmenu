229962cad964d3142e420ecdf5d956ef9fe68624
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-01-18 23:18:13 +1000

    [PATCH] dmenu-fuzzymatch-4.9.diff

--- config.def.h
+++ config.def.h
@@ -1,29 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b option; if 0, dmenu appears at bottom */
+static int fuzzy  = 1; /* -F option; if 0, dmenu doesn't use fuzzy matching */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
+static       char font[]   = "monospace:size=10";
 static const char *fonts[] = {
 	font,
-	"monospace:size=10",
+	"monospace:size=10"
 };
 
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static char *prompt = NULL; /* -p option; prompt to the left of input field */
 
-static char normfgcolor[] = "#bbbbbb";
-static char normbgcolor[] = "#222222";
-static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
+static char col_norm_fg[] = "#bbbbbb";
+static char col_norm_bg[] = "#222222";
+static char col_sel_fg[]  = "#eeeeee";
+static char col_sel_bg[]  = "#005577";
+static char col_out_fg[]  = "#000000";
+static char col_out_bg[]  = "#00ffff";
 static char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { normfgcolor, normbgcolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { "#000000",   "#00ffff" },
+	/*               fg           bg          */
+	[SchemeNorm] = { col_norm_fg, col_norm_fg },
+	[SchemeSel]  = { col_sel_fg,  col_sel_bg  },
+	[SchemeOut]  = { col_out_fg,  col_out_bg   },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -31,16 +35,19 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 
-/*
- * Xresources preferences to load at startup
- */
+/* Xresources preferences to load at startup */
 ResourcePref resources[] = {
-	{ "font",        STRING, &font },
-	{ "normfgcolor", STRING, &normfgcolor },
-	{ "normbgcolor", STRING, &normbgcolor },
-	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "selbgcolor",  STRING, &selbgcolor },
-	{ "prompt",      STRING, &prompt },
+	{ "topbar",      INTEGER, &topbar      },
+	{ "fuzzy",       INTEGER, &fuzzy       },
+	{ "font",        STRING,  &font        },
+	{ "prompt",      STRING,  &prompt      },
+	{ "col_norm_fg", STRING,  &col_norm_fg },
+	{ "col_norm_bg", STRING,  &col_norm_bg },
+	{ "col_sel_fg",  STRING,  &col_sel_fg  },
+	{ "col_sel_bg",  STRING,  &col_sel_bg  },
+	{ "col_out_fg",  STRING,  &col_out_fg  },
+	{ "col_out_bg",  STRING,  &col_out_bg  },
+	{ "lines",       INTEGER, &lines       },
 };
 
 /* vim:set ts=4 sts=4 sw=4 */
